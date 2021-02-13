d7a7f324ffa69450822b7d2f127658601b4abeb6
Zulfilham <code.zulfilham@gmail.com>
2021-02-13 12:23:00 +0700

    Ubah gaya penulisan, perbaiki kesalahan tik

--- finance.php
+++ finance.php
@@ -1,44 +1,44 @@
 <?php
 
-class finance {
+class Finance {
    private $data = [["nominal" => 500, "count" => 0], ["nominal" => 1000, "count" => 0], ["nominal" => 1000, "count" => 0], ["nominal" => 2000, "count" => 0], ["nominal" => 5000, "count" => 0], ["nominal" => 10000, "count" => 0], ["nominal" => 20000, "count" => 0], ["nominal" => 50000, "count" => 0], ["nominal" => 100000, "count" => 0]];
    private $data_filename = __DIR__ . "/.cache.dat";
    private $total = 0;
    private $succeed, $error_message;
 
-   public function __construct () {
-      $this->load_data ();
-      $this->count_total ();
+   public function __construct() {
+      $this->load_data();
+      $this->count_total();
    }
 
-   private function cache_data () {
-      file_put_contents ($this->data_filename, serialize ($this->data));
+   private function cache_data() {
+      file_put_contents($this->data_filename, serialize($this->data));
    }
 
-   private function load_data () {
-      if (file_exists ($this->data_filename) and $contents = @unserialize (file_get_contents ($this->data_filename)))
+   private function load_data() {
+      if (file_exists($this->data_filename) and $contents = @unserialize(file_get_contents($this->data_filename)))
          $this->data = $contents;
    }
 
-   public function get_data () {
+   public function get_data() {
       return $this->data;
    }
 
-   private function count_total () {
+   private function count_total() {
       $this->total = 500 * $this->data[0]["count"] + 1000 * $this->data[1]["count"] + 1000 * $this->data[2]["count"] + 2000 * $this->data[3]["count"] + 5000 * $this->data[4]["count"] + 10000 * $this->data[5]["count"] + 20000 * $this->data[6]["count"] + 50000 * $this->data[7]["count"] + 100000 * $this->data[8]["count"];
    }
 
-   public function get_total () {
+   public function get_total() {
       return $this->total;
    }
 
-   public function revenue ($arguments) {
+   public function revenue($arguments) {
       $initial_data = $this->data;
 
       foreach ($arguments as $pointer => $argument) {
          $nth = $pointer + 2;
 
-         if (preg_match ("!^(500{1,3}|1000{1,3}|2000{1,2})(=|:)([1-9][0-9]*)$!", $argument, $items)) {
+         if (preg_match("!^(500{1,3}|1000{1,3}|2000{1,2})(=|:)([1-9][0-9]*)$!", $argument, $items)) {
             if ($items[2] === ":") {
                switch ($items[1]) {
                   case "500":
@@ -90,14 +90,14 @@ class finance {
          }
       }
 
-      if (empty ($arguments)) {
+      if (empty($arguments)) {
          $this->succeed = false;
          $this->error_message = "Eror fatal: tidak ada argumen pada opsi -r.\n";
       }
-      elseif ($this->confirm_revenue ($initial_data, $this->data)) {
+      elseif ($this->confirm_revenue($initial_data, $this->data)) {
          $this->succeed = true;
-         $this->count_total ();
-         $this->cache_data ();
+         $this->count_total();
+         $this->cache_data();
       }
       else {
          $this->succeed = false;
@@ -105,14 +105,14 @@ class finance {
       }
    }
 
-   public function spending ($arguments) {
+   public function spending($arguments) {
       $initial_data = $this->data;
-      $counts = array_column ($this->data, "count");
+      $counts = array_column($this->data, "count");
 
       foreach ($arguments as $pointer => $argument) {
          $nth = $pointer + 2;
 
-         if (preg_match ("!^(500{1,3}|1000{1,3}|2000{1,2})(=|:)([1-9][0-9]*)$!", $argument, $items)) {
+         if (preg_match("!^(500{1,3}|1000{1,3}|2000{1,2})(=|:)([1-9][0-9]*)$!", $argument, $items)) {
             if ($items[2] === ":") {
                switch ($items[1]) {
                   case "500":
@@ -157,7 +157,7 @@ class finance {
                }
             }
 
-            if (isset ($i)) {
+            if (isset($i)) {
                $this->succeed = false;
                $this->error_message = "Eror fatal: nominal '{$this->data[$i]["nominal"]}' pada paramter ke $nth hanya '{$this->data[$i]["count"]}', tidak cukup.\n";
                return null;
@@ -170,14 +170,14 @@ class finance {
          }
       }
 
-      if (empty ($arguments)) {
+      if (empty($arguments)) {
          $this->succeed = false;
          $this->error_message = "Eror fatal: tidak ada argumen pada opsi -s.\n";
       }
-      elseif ($this->confirm_spending ($initial_data, $this->data)) {
+      elseif ($this->confirm_spending($initial_data, $this->data)) {
          $this->succeed = true;
-         $this->count_total ();
-         $this->cache_data ();
+         $this->count_total();
+         $this->cache_data();
       }
       else {
          $this->succeed = false;
@@ -185,28 +185,27 @@ class finance {
       }
    }
 
-   public function get_succeed () {
+   public function get_succeed() {
       return $this->succeed;
    }
 
-   public function get_error_message () {
+   public function get_error_message() {
       return $this->error_message;
    }
 
-   private function confirm_revenue ($initial_data, $final_data) {
+   private function confirm_revenue($initial_data, $final_data) {
       $prompt = "\e[1m***\e[0m \e[34;1mFINANCE\e[0m \e[1m***\e[0m\n    \e[34;1mAnda akan menambahkan uang\e[0m:\n";
       $total = 0;
 
       foreach ($initial_data as $i => $init_data) {
          if ($init_data["count"] !== $final_data[$i]["count"]) {
             $multiplier = $final_data[$i]["count"] - $init_data["count"];
-            $prompt .= sprintf ("        \e[34;1m%d\e[0m (\e[34;1m%d\e[0m)\n", $init_data["nominal"], $multiplier);
+            $prompt .= sprintf("        \e[34;1m%d\e[0m (\e[34;1m%d\e[0m)\n", $init_data["nominal"], $multiplier);
             $total += $init_data["nominal"] * $multiplier;
          }
       }
 
-      printf ("%s        \e[34;1mTOTAL\e[0m: Rp %s\n\napa anda yakin (Y/n) > ", $prompt, format_number ($total));
-      $input = readline ();
+      $input = readline(sprintf("%s        \e[34;1mTOTAL\e[0m: Rp %s\n\napa anda yakin (Y/n) > ", $prompt, format_number($total)));
 
       if ($input === "y" or $input === "Y")
          return true;
@@ -214,20 +213,19 @@ class finance {
          return false;
    }
 
-   private function confirm_spending ($initial_data, $final_data) {
+   private function confirm_spending($initial_data, $final_data) {
       $prompt = "\e[1m***\e[0m \e[34;1mFINANCE\e[0m \e[1m***\e[0m\n    \e[34;1mAnda akan mengeluarkan uang\e[0m:\n";
       $total = 0;
 
       foreach ($initial_data as $i => $init_data) {
          if ($init_data["count"] !== $final_data[$i]["count"]) {
             $multiplier = $init_data["count"] - $final_data[$i]["count"];
-            $prompt .= sprintf ("        \e[34;1m%d\e[0m (\e[34;1m%d\e[0m)\n", $init_data["nominal"], $multiplier);
+            $prompt .= sprintf("        \e[34;1m%d\e[0m (\e[34;1m%d\e[0m)\n", $init_data["nominal"], $multiplier);
             $total += $init_data["nominal"] * $multiplier;
          }
       }
 
-      printf ("%s        \e[34;1mTOTAL\e[0m: Rp %s\n\napa anda yakin (Y/n) > ", $prompt, format_number ($total));
-      $input = readline ();
+      $input = readline(sprintf("%s        \e[34;1mTOTAL\e[0m: Rp %s\n\napa anda yakin (Y/n) > ", $prompt, format_number($total)));
 
       if ($input === "y" or $input === "Y")
          return true;
@@ -236,92 +234,93 @@ class finance {
    }
 }
 
-function format_number ($number) {
-   $reversed_number = strrev ($number);
-   $numbers = str_split ($reversed_number, 3);
-   $reversed_glued_number = implode (".", $numbers);
-   return strrev ($reversed_glued_number);
+function format_number($number) {
+   $reversed_number = strrev($number);
+   $numbers = str_split($reversed_number, 3);
+   $reversed_glued_number = implode(".", $numbers);
+   return strrev($reversed_glued_number);
 }
 
-function list_finance (finance $finance) {
-   $data = $finance->get_data ();
-   $i = strlen (max (array_column ($data, "count"))) + 2;
+function list_finance(Finance $finance) {
+   $data = $finance->get_data();
+   $i = strlen(max(array_column($data, "count"))) + 2;
    echo "\e[1m***\e[0m \e[34;1mFINANCE\e[0m \e[1m***\e[0m\n";
-   printf ("    \e[34;1m%-4s\e[0m: \e[1m%-${i}s\e[0m \e[34;1m%-6s\e[0m: \e[1m%s\e[0m\n", $data[0]["nominal"], $data[0]["count"], $data[5]["nominal"], $data[5]["count"]);
-   printf ("    \e[34;1m%-4s\e[0m: \e[1m%-${i}s\e[0m \e[34;1m%-6s\e[0m: \e[1m%s\e[0m\n", $data[1]["nominal"], $data[1]["count"], $data[6]["nominal"], $data[6]["count"]);
-   printf ("    \e[34;1m%-4s\e[0m: \e[1m%-${i}s\e[0m \e[34;1m%-6s\e[0m: \e[1m%s\e[0m\n", $data[2]["nominal"], $data[2]["count"], $data[7]["nominal"], $data[7]["count"]);
-   printf ("    \e[34;1m%-4s\e[0m: \e[1m%-${i}s\e[0m \e[34;1m%-6s\e[0m: \e[1m%s\e[0m\n", $data[3]["nominal"], $data[3]["count"], $data[8]["nominal"], $data[8]["count"]);
-   printf ("    \e[34;1m%-4s\e[0m: \e[1m%s\e[0m\n\n", $data[4]["nominal"], $data[4]["count"]);
-   printf ("    \e[34;1mTOTAL\e[0m: \e[1mRp %s\e[0m\n", format_number ($finance->get_total ()));
+   printf("    \e[34;1m%-4s\e[0m: \e[1m%-${i}s\e[0m \e[34;1m%-6s\e[0m: \e[1m%s\e[0m\n", $data[0]["nominal"], $data[0]["count"], $data[5]["nominal"], $data[5]["count"]);
+   printf("    \e[34;1m%-4s\e[0m: \e[1m%-${i}s\e[0m \e[34;1m%-6s\e[0m: \e[1m%s\e[0m\n", $data[1]["nominal"], $data[1]["count"], $data[6]["nominal"], $data[6]["count"]);
+   printf("    \e[34;1m%-4s\e[0m: \e[1m%-${i}s\e[0m \e[34;1m%-6s\e[0m: \e[1m%s\e[0m\n", $data[2]["nominal"], $data[2]["count"], $data[7]["nominal"], $data[7]["count"]);
+   printf("    \e[34;1m%-4s\e[0m: \e[1m%-${i}s\e[0m \e[34;1m%-6s\e[0m: \e[1m%s\e[0m\n", $data[3]["nominal"], $data[3]["count"], $data[8]["nominal"], $data[8]["count"]);
+   printf("    \e[34;1m%-4s\e[0m: \e[1m%s\e[0m\n\n", $data[4]["nominal"], $data[4]["count"]);
+   printf("    \e[34;1mTOTAL\e[0m: \e[1mRp %s\e[0m\n", format_number($finance->get_total()));
 }
 
-function error_finance (finance $finance) {
-   fwrite (STDERR, $finance->get_error_message ());
-   exit (1);
+function error_finance(Finance $finance) {
+   fwrite(STDERR, $finance->get_error_message());
+   exit(1);
 }
 
-function log_data ($data, $status)  {
+function log_data($data, $status)  {
    $log_filename = __DIR__ . "/.cache.log";
-   $contents = str_replace ("none", $status, $data);
+   $contents = str_replace("none", $status, $data);
 
-   if (file_put_contents ($log_filename, $contents, FILE_APPEND))
+   if (file_put_contents($log_filename, $contents, FILE_APPEND))
       return true;
    else
       return false;
 }
 
-function main ($arguments, $count_arguments) {
-   $finance = new finance ();
+function main($arguments, $count_arguments) {
+   $finance = new Finance();
    $searches = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "January", "February", "March", "May", "June", "July", "August", "October", "December"];
    $replacements = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Januari", "Februari", "Maret", "Mei", "Juni", "Juli", "Agustus", "Oktober", "Desember"];
-   date_default_timezone_set ("Asia/Jakarta");
-   $date = str_replace ($searches, $replacements, date ("l, j F Y - H:i:s"));
-   $command = "finance " . implode (" ", array_slice ($arguments, 1));
+   date_default_timezone_set("Asia/Jakarta");
+   $date = str_replace($searches, $replacements, date("l, j F Y - H:i:s"));
+   $command = "finance " . implode(" ", array_slice($arguments, 1));
    $log_data = "$date -> $command\nstatus: none\n\n";
 
    if ($count_arguments === 1) {
-      list_finance ($finance);
-      log_data ($log_data, "true");
+      list_finance($finance);
+      log_data($log_data, "true");
    }
    else {
       switch ($arguments[1]) {
       case "-r":
-         $finance->revenue (array_slice ($arguments, 2));
+         $finance->revenue(array_slice($arguments, 2));
 
-         if ($finance->get_succeed ()) {
+         if ($finance->get_succeed()) {
             echo "Pemasukkan uang berhasil dilakukan.\n";
-            log_data ($log_data, "true");
+            log_data($log_data, "true");
          }
          else {
-            log_data ($log_data, "false");
-            error_finance ($finance);
+            log_data($log_data, "false");
+            error_finance($finance);
          }
          break;
       case "-s":
-         $finance->spending (array_slice ($arguments, 2));
+         $finance->spending(array_slice($arguments, 2));
 
-         if ($finance->get_succeed ()) {
+         if ($finance->get_succeed()) {
             echo "Pengeluaran uang berhasil dilakukan.\n";
-            log_data ($log_data, "true");
+            log_data($log_data, "true");
          }
          else {
-            log_data ($log_data, "false");
-            error_finance ($finance);
+            log_data($log_data, "false");
+            error_finance($finance);
          }
          break;
       case "-l":
-         list_finance ($finance);
-         log_data ($log_data, "true");
+         list_finance($finance);
+         log_data($log_data, "true");
          break;
       case "-h":
-echo <<< 'help'
-finance: A simple financial manager.
-    Usage: finance [-lh]
-           finance [-s | -r] <format>...
+echo <<< help
+Usage: finance [-lh]
+       finance [-s | -r] <format>...
+
+A simple financial manager.
 
 option:
-    -r <format>,   set money to be a revenue.
-    -s <format>,   set money to be spending out.
+    -r <format>   set money to be a revenue.
+    -s <format>   set money to be spending out.
 
 format:
    The format contains 2 arguments: nominal, and count. They are merged
@@ -332,19 +331,21 @@ format:
       500, 1000, 2000, 5000, 10000, 20000, 50000, 100000.
    count:
       greater than 0.
+
 AUTHOR: Zulfilham\n
 help;
-         log_data ($log_data, "true");
+         log_data($log_data, "true");
          break;
       default:
-         fwrite (STDERR, "Eror fatal: argumen '$arguments[1]' tidak valid.\n");
-         log_data ($log_data, "error");
-         exit (1);
+         fwrite(STDERR, "Eror fatal: argumen '$arguments[1]' tidak valid.\n");
+         log_data($log_data, "error");
+         exit(1);
          break;
       }
    }
 }
 
-main ($argv, $argc);
+main($argv, $argc);
 
 ?>
+
